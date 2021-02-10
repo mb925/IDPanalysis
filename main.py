8f9ad6c86ebeb0c906f8653011cd8ebfce0c6502
martina <martina.bevilacqua1992@gmail.com>
2021-02-10 17:34:53 +0100

    work on sequence-regions alignments

--- main.py
+++ main.py
@@ -5,31 +5,47 @@ curdir = pathlib.Path().absolute()
 curpath = str(curdir) + "/disordered_sequences_splitted_fasta"
 curpathreg = str(curdir) + "/disordered_regions_splitted_fasta"
 # outfile = 'alignments_global_needle/global_triangular_matrix.csv'
-outfile = 'alignments_global_needle/regseq_triangular_matrix.csv'
+outfilereg = 'alignments_global_needle/regseq_triangular_matrix.csv'
+
+
+# outfile = 'alignments_global_needle/regseq_triangular_matrix.csv'
+# create the couples
+def create_pairwise_regseq(dir1, dir2, outfile):
+
+    with open(outfile, 'w+') as regseq_triangular_matrix:
+        for uniprot in os.listdir(dir1):
+            for region in os.listdir(dir2):
+                regseq_triangular_matrix.write(uniprot.split('.')[0] + '\t' + region.split('.')[0] + '\n')
+
+
+
+# outfile = 'alignments_global_needle/global_triangular_matrix.csv'
 # create the couples
 # creating a triangular matrix
-def create_pairwise_sequences(dir1,dir2, outfile):
+def create_pairwise_sequences(dir1, outfile):
+    pairwise_sequences = {}
 
-  totalfiles = []
-  # pairwise_sequences = {}
+    with open(outfile, 'w') as global_triangular_matrix:
 
-  with open(outfile, 'w') as global_triangular_matrix:
+        for filename1 in os.listdir(dir1):
 
-    for filename1 in os.listdir(dir1):
+            i = os.listdir(curpath).index(filename1)
+            print(i)
+            for filename2 in os.listdir(curpath)[i:-1]:
+                print(filename2)
 
-      totalfiles.append(filename1.split(".")[0])
-      # i = os.listdir(curpath).index(filename1)
-      # for filename2 in os.listdir(curpath)[i:-1]:
-      for filename2 in os.listdir(dir2):
-        # if pairwise_sequences.get(filename1.split(".")[0], -1) == -1:
-        #   pairwise_sequences[filename1.split(".")[0]] = []
-        # else:
-        #   pairwise_sequences[filename1.split(".")[0]].append(filename2.split(".")[0])
-        if filename1.split(".")[0] != filename2.split(".")[0]:
-          global_triangular_matrix.write(filename1.split(".")[0] + '\t' + filename2.split(".")[0] + '\n')
+                # for filename2 in os.listdir(dir2): # for squared matrix
+                if pairwise_sequences.get(filename1.split(".")[0], -1) == -1:
+                    pairwise_sequences[filename1.split(".")[0]] = []
+                    global_triangular_matrix.write(filename1.split(".")[0] + '\t' + filename2.split(".")[0] + '\n')
+
+                else:
+                    pairwise_sequences[filename1.split(".")[0]].append(filename2.split(".")[0])
+                    # if filename1.split(".")[0] != filename2.split(".")[0]:
+                    global_triangular_matrix.write(filename1.split(".")[0] + '\t' + filename2.split(".")[0] + '\n')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-  create_pairwise_sequences(curpath, curpathreg, outfile)
-
-
+    # create_pairwise_sequences(curpath, outfile)
+    create_pairwise_regseq(curpath, curpathreg, outfilereg)
--- parser/parse.py
+++ parser/parse.py
@@ -1,81 +1,115 @@
+#!/usr/bin/env python3
+
 import argparse
 import os
 import sys
+import re
 
 def main():
-  parse_file()
+    parse_file()
 
 
 def parse_file():
-  curdir = os.getcwd()
-  args = parse_arguments()
+    args = parse_arguments()
+    scriptdir = os.path.dirname(os.path.realpath(__file__))
+
+    id1 = ''
+    id2 = ''
+    seq1 = ''
+    seq2 = ''
+    len1 = ''
+    reg1 = []
+    reg2 = []
+    disprotid1 = ''
+    disprotid2 = ''
+    uniprot = ''
+    region = ''
+    start = ''
+    end = ''
+    if args.i:
+        inf = open(args.i, 'r')
+    else:
+        inf = sys.stdin
+
+    regexpUniProt = re.compile('^[OPQopq][0-9][A-Za-z0-9]{3}[0-9]|[A-Na-nR-Zr-z][0-9]([A-Za-z][A-Za-z0-9]{2}[0-9]){1,2}$')  # seqres
+    for line in inf:
+        if line.startswith("# 1:"):
+            id1 = line.split(":")[1].strip()
+            if regexpUniProt.match(id1) != None: # seqres
+                uniprot = id1
+            else:
+                region = id1
+
+        elif line.startswith("# 2:"):
+            id2 = line.split(":")[1].strip()
+
+            if regexpUniProt.match(id2) != None:
+                uniprot = id2
+            else:
+                region = id2
+
+        elif line.startswith("# Identity:"):
+            identity = line.split(":")[1].strip()
+            identity = identity.split('(')[0].strip()
+        elif line.startswith("# Similarity:"):
+            similarity = line.split(":")[1].strip()
+            similarity = similarity.split('(')[0].strip()
+        elif line.startswith("# Gaps:"):
+            gaps = line.split(":")[1].strip()
+            gaps = gaps.split('(')[0].strip()
+        elif line.startswith("# Score:"):
+            score = line.split(":")[1].strip()
+        elif id1 != '' and line.startswith(id1):
+            sequence = line.split(id1)[1]
+            seq1 += ''.join([i for i in sequence if not i.isdigit()]).strip()
+        elif id2 != '' and line.startswith(id2):
+            sequence = line.split(id2)[1]
+            seq2 += ''.join([i for i in sequence if not i.isdigit()]).strip()
+
+    if inf is not sys.stdin:
+        inf.close()
 
 
-  id1 = ''
-  id2 = ''
-  seq1 = ''
-  seq2 = ''
-  len1 = ''
-  with open(args.i, 'r') as input:
-    for line in input:
-      if line.startswith("# 1:"):
+    len1 = str(len(seq1.replace("-", "")))
+    len2 = str(len(seq2.replace("-", "")))
 
-        id1 = line.split(":")[1].strip()
-        continue
-      elif line.startswith("# 2:"):
-        id2 = line.split(":")[1].strip()
-        continue
-      elif line.startswith("# Identity:"):
-        identity = line.split(":")[1].strip()
-        identity = identity.split('(')[0].strip()
-        continue
-      elif line.startswith("# Similarity:"):
-        similarity = line.split(":")[1].strip()
-        similarity = similarity.split('(')[0].strip()
-        continue
-      elif line.startswith("# Gaps:"):
-        gaps = line.split(":")[1].strip()
-        gaps = gaps.split('(')[0].strip()
-        continue
-      elif line.startswith("# Score:"):
-        score = line.split(":")[1].strip()
-        continue
-      elif id1 != '' and line.startswith(id1):
-        sequence = line.split(id1)[1]
-        seq1 += ''.join([i for i in sequence if not i.isdigit()]).strip()
-        continue
-      elif id2 != '' and line.startswith(id2):
-        sequence = line.split(id2)[1]
-        seq2 += ''.join([i for i in sequence if not i.isdigit()]).strip()
-        continue
 
-    with open(curdir + '/disordered_sequences_splitted_fasta/' + id1 + '.fasta', 'r') as inputseq:
-      for row in inputseq:
-        if not row.startswith('>'):
-          len1 = str(len(row))
-          break
+    with open(scriptdir + '/../sequences_regions/search_in_disprot.tsv', 'r') as input:
+        for row in input:
 
-    with open(curdir + '/disordered_sequences_splitted_fasta/' + id2 + '.fasta', 'r') as inputseq:
-      for row in inputseq:
-        if not row.startswith('>'):
-          len2 = str(len(row))
-          break
 
-  out = id1 + ',' + id2 + ',' + len1 + ',' + len2 + ',' + identity + ',' + similarity + ',' + gaps + ',' + score + ',' + seq1 + ',' + seq2
-  print(out) # script output
+            # if id1 == row.split('\t')[0]:
 
+            if uniprot == row.split('\t')[0]:
+
+                disprotid1 = row.split('\t')[4]
+                reg1.append(row.split('\t')[5] + '_' + start + '-' + end)
+                ##TODO
+                # if region =
+                # start = row.split('\t')[6]  # seqres
+                # end = row.split('\t')[7]  # seqres
+            # elif id2 == row.split('\t')[0]:
+                # disprotid2 = row.split('\t')[4]
+                # reg2.append(row.split('\t')[5] + '_' + start + '-' + end)
+    reg1 = ','.join(reg1)
+    reg2 = ','.join(reg2)
+    # print(reg1)
+    # print(reg2)
+    out = "\t".join([disprotid1, uniprot, region.split('_')[0], len1, len2, identity, similarity, gaps, score, seq1, seq2, start, end, reg1, region]) # seqres
+    # out = "\t".join([disprotid1, disprotid2, id1, id2, len1, len2, identity, similarity, gaps, score, seq1, seq2, reg1, reg2])
+    print(out)  # script output
 
 def parse_arguments(initargs=None):
-  if initargs is None:
-    initargs = []
-  parser = argparse.ArgumentParser(description="Parse input alignment file.")
-  parser.add_argument('-i', required=True, help="Input alignment file")
-  if not initargs or len(sys.argv) > 1:
-    args = parser.parse_args()
-  else:
-    args = parser.parse_args(initargs)
-  return args
+    if initargs is None:
+        initargs = []
+    parser = argparse.ArgumentParser(description="Parse input alignment file.")
+    parser.add_argument('-i', help="Input alignment file")
+    if not initargs or len(sys.argv) > 1:
+        args = parser.parse_args()
+    else:
+        args = parser.parse_args(initargs)
+    return args
 
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
