bcbe3b33f31f3a0ad744cc550605732deae4646a
martina <martina.bevilacqua1992@gmail.com>
2021-03-12 18:10:50 +0100

    refactored with pandas

--- rearrange/rearrange.py
+++ rearrange/rearrange.py
@@ -15,117 +15,99 @@ def main():
 def create_tables():
 
     scriptdir = os.path.dirname(os.path.realpath(__file__))
+    # with open(scriptdir + '/outtest.tsv', 'w+') as out:
+    with open(scriptdir + '/all-dataframe.tsv', 'w+') as out:
+        out.write('id1' + '\t' + 'id2' + '\t' + 's1' + '\t' + 's2' + '\t' + 'p1' + '\t' + 'p2' + '\t' + 'd1' + '\t' + 'd2' + '\n')
+        count = 0
+        # with open(scriptdir + '/testinp.tsv', 'r') as alignments:
 
+        with open(scriptdir + '/../clustering/components-filtered.tsv', 'r') as alignments:
+            next(alignments)
 
-    with open(scriptdir + '/../clustering/components-filtered.tsv', 'r') as alignments:
-        for line in alignments:
+            for line in alignments:
+                d = {'id1': [], 'id2': [], 's1': [], 'p1': [], 'p2': [], 's2': [], 'd1': [], 'd2': []}
 
-            d = {'s1': [], 'p1': [], 'p2': [], 's2': [], 'd1': [], 'd2': []}
-            id1 = line.split('\t')[2]
-            id2 = line.split('\t')[3]
-            align1 = ''
-            align2 = ''
-            alignoutput = line.split('#=')[1].split('/n')
-            for row in alignoutput:
-                if row.startswith(id1 + ' '):
-                    row = row.split(id1 + ' ')[1].strip()
-                    row = ''.join([i for i in row if not i.isdigit()]).strip()
-                    align1 += row
-            for row in alignoutput:
-                if row.startswith(id2 + ' '):
-                    row = row.split(id2 + ' ')[1].strip()
-                    row = ''.join([i for i in row if not i.isdigit()]).strip()
-                    align2 += row
+                id1 = line.split('\t')[2]
+                id2 = line.split('\t')[3]
+                align1 = ''
+                align2 = ''
+                alignoutput = line.split('#=')[1].split('/n')
+                for row in alignoutput:
+                    if row.startswith(id1 + ' '):
+                        row = row.split(id1 + ' ')[1].strip()
+                        row = ''.join([i for i in row if not i.isdigit()]).strip()
+                        align1 += row
+                for row in alignoutput:
+                    if row.startswith(id2 + ' '):
+                        row = row.split(id2 + ' ')[1].strip()
+                        row = ''.join([i for i in row if not i.isdigit()]).strip()
+                        align2 += row
 
-            i = 0
-            j = 1
+                i = 0
+                j = 1
 
-            if len(align1) == len(align2):
-                while i < len(align1):
-                    # print(align1[i] + '_' + str(j))
-                    if align1[i] != '-':
-                        d['s1'].append(align1[i])
-                        d['p1'].append(str(j))
-                    elif align1[i] == '-':
-                        d['s1'].append('-')
-                        d['p1'].append('na')
+                if len(align1) == len(align2):
+                    while i < len(align1):
+                        d['id1'].append(id1)
+                        d['id2'].append(id2)
+                        if align1[i] != '-':
+                            d['s1'].append(align1[i])
+                            d['p1'].append(str(j))
+                        elif align1[i] == '-':
+                            d['s1'].append('-')
+                            d['p1'].append('') # na
 
-                    if align2[i] != '-':
-                        d['s2'].append(align2[i])
-                        d['p2'].append(str(j))
-                    elif align2[i] == '-':
-                        d['s2'].append('-')
-                        d['p2'].append('na')
+                        if align2[i] != '-':
+                            d['s2'].append(align2[i])
+                            d['p2'].append(str(j))
+                        elif align2[i] == '-':
+                            d['s2'].append('-')
+                            d['p2'].append('')
 
-                    j += 1
-                    i += 1
-            else:
-                print("Alignment to be checked")
-                print(id1)
-                print(id2)
-                print(align1)
-                print(align2)
-
-            reg1 = expand_regions(line.split('\t')[12].split(','))
-            reg2 = expand_regions(line.split('\t')[13].split(','))
-
-            # print(reg1)
-
-            for index, el in enumerate(d['s1']):
-                # print(d['p1'][index])
-                if el == '-': # if you have a gap you can't have disprot region
-                    d['d1'].append('na')
-                elif int(d['p1'][index]) in reg1:
-                    d['d1'].append(str(1))
+                        j += 1
+                        i += 1
                 else:
-                    d['d1'].append(0)
+                    print("Alignment to be checked")
+                    print(id1)
+                    print(id2)
+                    print(align1)
+                    print(align2)
 
-            for index, el in enumerate(d['s2']):
-                if el == '-':
-                    d['d2'].append('na')
-                elif int(d['p2'][index]) in reg2:
-                    d['d2'].append(str(1))
-                else:
-                    d['d2'].append(0)
+                reg1 = expand_regions(line.split('\t')[12].split(','))
+                reg2 = expand_regions(line.split('\t')[13].split(','))
 
-            title = id1 + '_' + id2
+                # print(reg1)
 
-            overlap_match, overlap_similar, union = calc_overlap_match_similar(d)
+                for index, el in enumerate(d['s1']):
+                    # print(d['p1'][index])
+                    if el == '-': # if you have a gap you can't have disprot region
+                        d['d1'].append('')
+                    elif int(d['p1'][index]) in reg1:
+                        d['d1'].append(1)
+                    else:
+                        d['d1'].append(0)
 
-            with open(scriptdir + '/rearranged_files/' + title + '.tsv', 'w+') as out:
-                out.write('>identity: ' + line.split('\t')[6] + '\n')
-                out.write('>overlap/union_match: ' + overlap_match + '/' + union + '\n')
-                out.write('>overlap/union_similar: ' + overlap_similar + '/' + union + '\n')
-                out.write('s1' + '\t' + 's2' + '\t' + 'p1' + '\t' + 'p2' + '\t' + 'd1' + '\t' + 'd2' + '\n')
-
-
-                counter = 0
-                while counter < len(d['s1']):
-                    out.write(d['s1'][counter] + '\t' + d['s2'][counter]  + '\t' + d['p1'][counter] + '\t' + d['p2'][counter] + '\t' + str(d['d1'][counter]) + '\t' + str(d['d2'][counter]) + '\n')
-                    counter += 1
+                for index, el in enumerate(d['s2']):
+                    if el == '-':
+                        d['d2'].append('')
+                    elif int(d['p2'][index]) in reg2:
+                        d['d2'].append(1)
+                    else:
+                        d['d2'].append(0)
 
 
+                count += 1
+                print(count)
 
-def calc_overlap_match_similar(df):
-    overlap_match = 0
-    union_match = 0
-    overlap_similar = 0
-    union = 0
-    for index, d1 in enumerate(df['d1']):
-        if d1 == '1' and df['d2'][index] == '1':
-            overlap_similar += 1
-            if df['s1'][index] == df['s2'][index]:
-                overlap_match += 1
-        if d1 == '1' or df['d2'][index] == '1':
-            union += 1
 
-    # if overlap_similar < overlap_match:
-    #     print(overlap_match)
-    #     print(overlap_similar)
-    # if union_similar < union_match:
-    #     print(union_match)
-    #     print(union_similar)
-    return [str(overlap_match), str(overlap_similar), str(union)]
+                x = 0
+                while x < len(d['id2']):
+
+                    out.write(d['id1'][x] + '\t' + d['id2'][x] + '\t' + d['s1'][x] + '\t' + d['s2'][
+                        x] + '\t' + d['p1'][x] + '\t' + d['p2'][x] + '\t' + str(d['d1'][x]) + '\t' + str(
+                        d['d2'][x]) + '\n')
+                    x += 1
+
 
 def expand_regions(regions):
     transformed_regions = []
--- visualizing/visualize.py
+++ visualizing/visualize.py
@@ -1,376 +1,148 @@
 #!/usr/bin/env python3
-import pandas as pd
-import statistics
-import matplotlib.pyplot as plt
-
-import argparse
 import numpy as np
 import os
 import sys
-
-
-from shutil import copyfile
 import matplotlib.pyplot as plt
-from pandas import DataFrame
+import pandas as pd
 
 
 def main():
-    scriptdir = os.path.dirname(os.path.realpath(__file__))
+    script_dir = os.path.dirname(os.path.realpath(__file__))
 
-    visualize_overlap_identity(scriptdir)
-    # visualize_identity(scriptdir)
-    # visualize_overlap(scriptdir)
-
-def visualize_overlap_identity(scriptdir):
-    x, y, z, union = [], [], [], []
-    count_mismatch = 0
-
-    for filename in os.listdir(scriptdir + '/../rearrange/rearranged_files'):
-        with open(scriptdir + '/../rearrange/rearranged_files/' + filename) as file:
-            for line in file:
-                overlap_match = 0
-                overlap_similar = 0
-
-                if line.startswith('>identity: '):
-                    identitynum = line.split('>identity: ')[1].replace('\n', '').split('/')[0]
-                    identity = round(int(identitynum) / int(line.split('>identity: ')[1].replace('\n', '').split('/')[1]), 2)
-                    identity = identity * 100
-
-                    x.append(identity)
-                if line.startswith('>overlap/union_match: '):
-
-                    numoverlap = int(line.split('>overlap/union_match: ')[1].split('/')[0])
-
-                    if numoverlap > 0:
-                        overlap_match = round(numoverlap / int(line.split('>overlap/union_match: ')[1].split('/')[1]), 2)
-                    y.append(overlap_match)
-                    # print(overlap_match)
-
-                if line.startswith('>overlap/union_similar: '):
-                    numoverlap = int(line.split('>overlap/union_similar: ')[1].split('/')[0])
-                    if numoverlap > 0:
-                        print(numoverlap)
-                        print(line)
-                        overlap_similar = round(numoverlap / int(line.split('>overlap/union_similar: ')[1].split('/')[1]), 2)
-                    union.append(int(line.split('>overlap/union_similar: ')[1].split('/')[1]))
-                    z.append(overlap_similar)
-                    continue
+    # visualize_overlap_identity(script_dir)
+    # visualize_overlap_identity_hist(script_dir)
+    visualize_alignments(script_dir)
 
 
-                # if identity >= 80 and overlap_similar <= 0.5:
-                #     copyfile(scriptdir + '/../rearrange/rearranged_files/' + filename, scriptdir + '/low_overlap_high_identity_mismatch/' + filename)
-                #     count_mismatch += 1
-                #
-                #     # print(overlap)
-    print('Low overlap high identity, total number of files: ' + str(count_mismatch))
+def visualize_overlap_identity(script_dir):
+    ident, over_mat, over_mis, union_similar = [], [], [], []
 
-    my_dpi = 50
-    fig = plt.figure( figsize=(20, 10), dpi=my_dpi)
-    print(fig)
+    df = pd.read_csv(script_dir + '/../rearrange/all-dataframe.tsv', sep='\t')
+    # print(df)
+    identity_df = pd.read_csv(script_dir + '/../clustering/components-filtered.tsv', sep='\t')
+
+    ids_identity = identity_df[['id1', 'id2', 'identity']].sort_values(by=['id1'])
+    union_similar = df.loc[(df.d2 == 1) | (df.d1 == 1)].groupby(['id1', 'id2']).size().to_frame('union-similar')
+
+    m1 = pd.merge(ids_identity, union_similar, on=['id1', 'id2'])
+    # print(m1)
+    overlap_similar = df.loc[(df.d2 == 1) & (df.d1 == 1)].groupby(['id1', 'id2']).size().to_frame('overlap-similar')
+    m2 = pd.merge(m1, overlap_similar, on=['id1', 'id2'], how='outer')
+    # print(m2)
+    overlap_match = df.loc[(df.d2 == 1) & (df.d1 == 1) & (df.s1 == df.s2)].groupby(['id1', 'id2']).size().to_frame(
+        'overlap-match')
+    m3 = pd.merge(m2, overlap_match, on=['id1', 'id2'], how='outer')
+    print(m3)
+
+    for value in m3['identity'].values:
+        num = int(value.split('/')[0])
+        den = int(value.split('/')[1])
+
+        value = 0
+        if num != 0:
+            value = (num / den) * 100
+        ident.append(round(value, 2))
+
+    m3['identity'] = m3['identity'].replace(m3['identity'].values, ident)
+
+    i = 0
+    while i < len(m3['overlap-similar'].values):
+        ov_sim = 0
+        ov_match = 0
+        num_sim = 0
+        num_match = 0
+        den = m3['union-similar'].values[i]
+
+        if not np.isnan(m3['overlap-similar'].values[i]):
+            num_sim = int(m3['overlap-similar'].values[i])
+        if not np.isnan(m3['overlap-match'].values[i]):
+            num_match = int(m3['overlap-match'].values[i])
+
+        if num_sim != 0:
+            ov_sim = round((num_sim / den), 2)
+        if num_match != 0:
+            ov_match = round((num_match / den), 2)
+        over_mis.append(ov_sim)
+        over_mat.append(ov_match)
+        i += 1
+
+    m3['overlap-similar/union'] = over_mis
+
+    m3.to_csv(script_dir + '/overlap-union-identity.csv', index=None, sep='\t')
+
+    fig = plt.figure(figsize=(20, 10), dpi=50)
 
     fig.suptitle('overlap/union', fontsize=24)
-    ax1 = fig.add_subplot(1, 2, 1)
-    # ax1 = fig.add_subplot(1, 3, 1)
-    ax1.set_title('overlap/union similar - overlap', fontsize=20)
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax1.set_title('colored by overlap match')
+    ax1.set_ylabel('overlap/union similar', fontsize=20)
     ax1.set_xlabel('identity', fontsize=20)
     cm = plt.cm.get_cmap('seismic')
-    sc = ax1.scatter(x, z, c=y, cmap=cm)
+    sc = ax1.scatter(ident, over_mis, c=over_mat, cmap=cm)
     plt.colorbar(sc)
+    plt.show()
 
-
-    # ax2 = fig.add_subplot(1, 3, 3)
-    ax2 = fig.add_subplot(1, 2, 2)
-    ax2.scatter(x, y)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax2.scatter(ident, over_mat)
     ax2.set_xlabel('identity', fontsize=20)
-    ax2.set_title('overlap/union match', fontsize=20)
+    ax2.set_ylabel('overlap/union match', fontsize=20)
 
-    # union = NormalizeData(union)
-    # print(union)
-    # ax3 = fig.add_subplot(1, 3, 2)
-    # sc3 = ax3.scatter(x, z, c=union, cmap=cm)
-    # plt.colorbar(sc3)
-    #
-    # ax3.set_xlabel('identity', fontsize=20)
-    # ax3.set_title('overlap/union similar - union', fontsize=20)
+    union = normalize_data(m3['union-similar'])
+    ax3 = fig.add_subplot(1, 3, 3)
+    sc3 = ax3.scatter(ident, over_mis, c=union, cmap=cm)
+    plt.colorbar(sc3)
 
-    fig.savefig('identity-overlap-new.png')
+    ax3.set_xlabel('identity', fontsize=20)
+    ax3.set_title('overlap/union similar - union similar', fontsize=20)
 
-def NormalizeData(data):
+    fig.savefig('identity-overlap.png')
+
+
+def normalize_data(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
 
-def visualize_identity(scriptdir):
 
+def visualize_overlap_identity_hist(script_dir):
     data = []
     dict = {}
     ids = []
     data_overlap = []
 
-    for filename in os.listdir(scriptdir + '/../rearrange/rearranged_files'):
-        with open(scriptdir + '/../rearrange/rearranged_files/' + filename) as file:
-            filename = filename.split('.')[0]
+    df = pd.read_csv(script_dir + '/overlap-union-identity.csv', sep='\t')
+    for i, id1 in enumerate(df['id1']):
+        data_overlap.append(df['overlap-similar/union'][i])
 
-            for line in file:
-                if line.startswith('>identity: '):
-                    identitynum = line.split('>identity: ')[1].replace('\n', '').split('/')[0]
-                    identity = round(
-                        int(identitynum) / int(line.split('>identity: ')[1].replace('\n', '').split('/')[1]), 2)
-                    identity = identity * 100
-                if line.startswith('>overlap/union_match: '):
-                    overlap = 0
-                    numoverlap = int(line.split('>overlap/union_match: ')[1].split('/')[0])
-                    if numoverlap > 0:
-                        overlap = round(numoverlap / int(line.split('>overlap/union_match: ')[1].split('/')[1]), 2)
-                    data_overlap.append(overlap)
-                if filename in dict:
-                    dict[filename.split('_')[0]].append(identity)
-                else:
-                    dict[filename.split('_')[0]] = []
-                    dict[filename.split('_')[0]].append(identity)
+        if id1 in dict:
+            dict[id1].append(df['identity'][i])
 
+        else:
+            dict[id1.split('_')[0]] = []
+            dict[id1.split('_')[0]].append(df['identity'][i])
+    print(dict)
     for key in dict:
-        maxval = max(dict[key])
+        max_val = max(dict[key])
         ids.append(key)
-        data.append(round(maxval, 2))
-    print(data_overlap)
-    # TODO add axes
-    # plt.hist(data, bins=20) # do not run both plots toghether or the results will overlap
+        data.append(round(max_val, 2))
+
+    print(sorted(data_overlap))
+
+    # plt.hist(data, bins=20) # !!!do not run both plots together or the results will overlap
+    # plt.xlabel('max identity')
+    # plt.ylabel('# proteins')
     # plt.gcf().savefig('max-identity.png')
 
+    plt.xlabel('overlap')
+    plt.ylabel('# pairs')
     plt.hist(data_overlap, bins=20)
-    plt.gcf().savefig('overlap-match-hist.png')
-
-# def visualize_identity(scriptdir):
-#
-#         data = []
-#         dict = {}
-#         ids = []
-#
-#         for filename in os.listdir(scriptdir + '/../rearrange/rearranged_files'):
-#             with open(scriptdir + '/../rearrange/rearranged_files/' + filename) as file:
-#                 filename = filename.split('.')[0]
-#
-#                 for line in file:
-#                     if line.startswith('>identity: '):
-#                         identitynum = line.split('>identity: ')[1].replace('\n', '').split('/')[0]
-#                         identity = round(
-#                             int(identitynum) / int(line.split('>identity: ')[1].replace('\n', '').split('/')[1]), 2)
-#                         identity = identity * 100
-#
-#                     elif line.startswith('>overlap/union: '):
-#                         overlap = 0
-#                         numoverlap = int(line.split('>overlap/union: ')[1].split('/')[0])
-#                         if numoverlap > 0:
-#                             overlap = round(numoverlap / int(line.split('>overlap/union: ')[1].split('/')[1]), 2)
-#                         if overlap >= 0.3:
-#
-#                             if filename in dict:
-#                                 dict[filename].append(identity)
-#                                 # dict[filename].append(overlap)
-#                             else:
-#                                 dict[filename] = []
-#                                 dict[filename].append(identity)
-#
-#                                 # dict[filename].append(overlap)
-#
-#         for key in dict:
-#             maxval = max(dict[key])
-#             ids.append(key)
-#             data.append(maxval)
-#         print(dict)
-#         i = 0
-#         lenids = len(ids)
-#         while i < lenids:
-#
-#             stop = i + 50
-#             subsetids = ids[i:stop]
-#             subsetdata = data[i:stop]
-#             y_pos = np.arange(len(subsetids))
-#
-#             # Create bars
-#             plt.bar(y_pos, subsetdata)
-#
-#             # Create names on the x-axis
-#             plt.xticks(y_pos, subsetids, rotation=90)
-#
-#             plt.savefig('hist_identity/subset' + str(i) + '.png')
-#             # plt.savefig('hist_overlap/subset' + str(i) + '.png')
-#             i += 50
-#             plt.clf()
-#             print(i)
+    plt.gcf().savefig('overlap-similar-hist.png')
 
 
-# def visualize_alignments(scriptdir):
-#
-#     with open(scriptdir + '/alignments.tsv', 'w+') as out:
-#         with open(scriptdir + '/../clustering/components-union-overlap.tsv', 'r') as input:
-#             for line in input:
-#
-#                 id2 = line.split('\t')[3].split(',')[0]
-#                 id1 = line.split('\t')[2].split(',')[0]
-#                 align1 = ''
-#                 align2 = ''
-#                 alignoutput = line.split('#=')[1].split('/n')
-#                 for row in alignoutput:
-#                     if row.startswith(id1):
-#                         row = row.split(id1)[1].strip()
-#                         row = ''.join([i for i in row if not i.isdigit()]).strip()
-#                         align1 += row
-#                 for row in alignoutput:
-#                     if row.startswith(id2):
-#                         row = row.split(id2)[1].strip()
-#                         row = ''.join([i for i in row if not i.isdigit()]).strip()
-#                         align2 += row
-#                 m = max([len(align1), len(align2)])
-#
-#                 # print(line)
-#                 print(line.split('\t')[15].split('/n'))
-#                 print(line.split('\t')[11].split('/n'))
-#                 reg1 = line.split('\t')[11].split(',')
-#                 reg1conv = []
-#                 for reg in reg1:
-#                     start1 = reg.split('_')[1].split('-')[0]
-#                     end1 = reg.split('_')[1].split('-')[1]
-#                     regconv = list(range(int(start1), int(end1)))
-#                     reg1conv.append(regconv)
-#                 j = 0
-#                 seq = ''
-#
-#                 # print(regconv)
-#                 if type(regconv[0]) is not int:
-#                     joined1 = []
-#                     for lista2 in reg2conv:
-#                         for el in lista2:
-#                             joined1.append(el)
-#                     while j < m:
-#
-#                         if j in joined1:
-#                             seq += '1'
-#                         else:
-#                             seq += '0'
-#                         j += 1
-#                 else:
-#                     while j < m:
-#
-#                         if j in regconv:
-#                             seq += '1'
-#                         else:
-#                             seq += '0'
-#                         j += 1
-#
-#                 print(line.split('\t')[12].split('/n'))
-#
-#                 reg2 = line.split('\t')[12].split(',')
-#                 reg2conv = []
-#                 for reg in reg2:
-#                     start2 = reg.split('_')[1].split('-')[0]
-#                     end2 = reg.split('_')[1].split('-')[1]
-#                     regconv2 = list(range(int(start2), int(end2)))
-#                     reg2conv.append(regconv2)
-#                 i = 0
-#                 seq2 = ''
-#                 # print(reg2conv)
-#                 # print( regconv2[len(regconv2)-1])
-#
-#                 if type(reg2conv[0]) is not int:
-#
-#                     i = 1
-#                     joined = []
-#                     for lista2 in reg2conv:
-#                         for el in lista2:
-#                             joined.append(el)
-#
-#
-#                     while i <= m:
-#
-#                         if i in joined:
-#                             seq2 += '1'
-#                         else:
-#                             seq2 += '0'
-#                         i += 1
-#
-#                 else:
-#                     while i <= m:
-#                             if i in reg2conv:
-#                                 seq2 += '1'
-#                             else:
-#                                 seq2 += '0'
-#                             i += 1
-#
-#
-#
-#
-#                 print(seq)
-#                 print(seq2)
-#                 k = 1
-#                 start = 0
-#                 end = 150
-#                 out.write("overlap/union: " + line.split('\t')[14].split('/n')[0] + '\n')
-#                 out.write("overlap/shortest region: " + line.split('\t')[15].split('/n')[0])
-#                 l = max([len(align1), len(align2), len(seq), len(seq2)])
-#                 out.write(id1 + ' ' + id2 + '\n')
-#                 out.write(str(reg1) + '\n')
-#                 out.write(str(reg2) + '\n')
-#
-#                 while k < l:
-#                     while start < l:
-#                         print(start)
-#                         print(end)
-#
-#                         out.write(seq[start:end] + '\n')
-#                         out.write(align1[start:end] + '\n')
-#                         out.write(align2[start:end] + '\n')
-#                         out.write(seq2[start:end] + '\n' + '\n')
-#                         start += 150
-#                         end += 150
-#                     k += 1
-
-
-# def visualize_overlap_identity(scriptdir):
-#
-#         flag = 0
-#         with open(scriptdir + '/../clustering/components-union-overlap.tsv', 'r') as input:
-#             x, y = [], []
-#             count = 0
-#
-#             for line in input:
-#                 id1 = line.split('\t')[0]
-#                 id2 = line.split('\t')[2]
-#                 count +=1
-#
-#                 identity = round(int(line.split('\t')[6].split('/')[0]) / int(
-#                         line.split('\t')[6].split('/')[1]), 2)
-#                 identity = identity * 100
-#                 # identity = int(line.split('\t')[6].split('/')[0])
-#                 if identity > flag:
-#                     flag = identity
-#
-#                 # numoverlap = int(line.split('\t')[15].split('/')[0])
-#                 numoverlap = int(line.split('\t')[14].split('/')[0])
-#                 overlap = 0
-#                 if numoverlap > 0:
-#
-#                     # overlap = round(int(line.split('\t')[15].split('/')[0]) / int(
-#                     #     line.split('\t')[15].split('/')[1]), 2)
-#                     overlap = round(int(line.split('\t')[14].split('/')[0]) / int(
-#                         line.split('\t')[14].split('/')[1]), 2)
-#
-#
-#                 # lenghts = statistics.mean([int(line.split('\t')[4]),int(line.split('\t')[5])] )
-#                 # print(count)
-#                 x.append(identity)
-#                 # x.append(identity)
-#                 # y.append(overlap)
-#                 y.append(overlap)
-#
-#             # print(y)
-#
-#
-#             plt.scatter(x, y, alpha=0.5)
-#             plt.xlabel("identity")
-#             # plt.ylabel("overlap/shortest region")
-#             plt.ylabel("overlap/union region")
-#             # plt.savefig('identity-region.png')
-#             plt.savefig('identity-overlap.png')
+def visualize_alignments(script_dir):
+    df = pd.read_csv(script_dir + '/../rearrange/all-dataframe.tsv', sep='\t')
+    m3 = pd.read_csv(script_dir + '/overlap-union-identity.csv', sep='\t')
+    improvable_regions = m3.loc[(m3['overlap-similar/union'] <= 0.5) & (m3['identity'] >= 80)][['id1', 'id2']]
+    bad_alignments_dataset = df.merge(improvable_regions, on=['id1', 'id2'], how='right')
+    bad_alignments_dataset.to_csv(script_dir + '/improvable_regions.csv', index=None, sep='\t')
 
 
 if __name__ == '__main__':
